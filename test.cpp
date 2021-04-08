6c1ae0d37d8d8bba7d12a8f326d22cc96f56a285
gokirstengo <kirstengo568@gmail.com>
2021-04-08 16:26:15 -0700

    added test cases to test.cpp

--- test.cpp
+++ test.cpp
@@ -1,29 +1,35 @@
+
 #include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
 
 TEST(EchoTest, HelloWorld) {
-    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
+    char* test_val[3]; test_val[0] = "./c-echo-count"; test_val[1] = "hello"; test_val[2] = "world";
     EXPECT_EQ("hello world", echo(3,test_val));
 }
+
 TEST(EchoTest, EmptyString) {
-    char* test_val[1]; test_val[0] = "./c-echo";
-    EXPECT_EQ("", echo(1,test_val));
+    char* test_val[1]; test_val[0] = "./c-echo-count";
+    EXPECT_EQ("", echo(1, test_val));
 }
-TEST(EchoTest, Burrito) {
-    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "I love Burritos"; test_val[2] = "I love Burritos";
-    EXPECT_EQ("I love Burritos I love Burritos", echo(3,test_val));
+
+TEST(CountTest, HelloWorld) {
+    std::string test_str = "hello world";
+    EXPECT_EQ(2, count(test_str));
 }
-TEST(EchoTest, Yeet) {
-    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "ya ya YEET"; test_val[2] = "yaya YEET";
-    EXPECT_EQ("ya ya YEET yaya YEET", echo(3,test_val));
+
+TEST(CountTest, EmptyString) {
+    std::string test_str = "";
+    EXPECT_EQ(0, count(test_str));
 }
-TEST(EchoTest, Valorant) {
-    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "I hate"; test_val[2] = "Viper buff";
-    EXPECT_EQ("I hate Viper buff", echo(3,test_val));
+
+TEST(CountTest, ManySpaces) {
+    std::string test_str = "   this   string has     weird   spacing";
+    EXPECT_EQ(5, count(test_str));
 }
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
